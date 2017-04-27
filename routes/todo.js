c43b89452f060452a1517a27d6c3f9b42619a448
alxgeoaln <georgianalinalexandru@gmail.com>
2017-04-27 19:27:33 +0300

    new toto route

--- routes/todo.js
+++ routes/todo.js
@@ -3,19 +3,18 @@ var passport = require('passport');
 var requireAuth = passport.authenticate('jwt', {session: false});
 var User = require('../models/user');
 
-router.post('/', function (req, res) {
+router.post('/', requireAuth, function (req, res) {
 
-    var userId = Object(req.body.userId);
+    var userId = req.user._id;
     var todo = {
-        text: req.body.text,
-        userId: req.body.userId
+        text: req.body.text
     };
 
     User.saveTodo(userId, todo, function (err, todo) {
         if (err)
-            console.log(err);
+            res.json({success: false, message: "Could not post todo"});
         else
-            res.json({success: true, message: 'Successfully added todo'});
+            res.json({todos: req.user.todo});
     })
 });
 

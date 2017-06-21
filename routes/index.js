b23cddd2cb5e64899d3e98d14fc7b176e83a6b8c
alxgeoaln <georgianalinalexandru@gmail.com>
2017-06-21 16:48:56 +0300

    asa

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var passport = require('passport');
 var passportService = require('../services/passport');
 var requireAuth = passport.authenticate('jwt', {session: false})
 
-router.get('/', function (req, res) {
-   res.json({message: works})
+router.get('/', requireAuth, function (req, res) {
+    res.send('Works')
 });
 
 module.exports = router;
